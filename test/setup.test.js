a141081739dc851817defa0a3dbf97d656298f88
klar2161 <56342242+klar2161@users.noreply.github.com>
2022-06-01 11:22:59 +0200

    Workflow maybe fixed

--- test/setup.test.js
+++ test/setup.test.js
@@ -1,12 +1,14 @@
 process.env.NODE_ENV = 'test';
 
 const Cat = require('../models/cat');
+const Dog = require('../models/dog');
 const User = require('../models/user');
 
 
 //clean up the database before and after each test
 beforeEach((done) => { 
     Cat.deleteMany({}, function(err) {});
+    Dog.deleteMany({}, function(err) {});
     User.deleteMany({}, function(err) {});
     done();
 });
